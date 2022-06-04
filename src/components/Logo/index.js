2774c2a44643cc7e401f24ee41d1fa2c18171055
Diwakar <diwakars652@gmail.com>
2022-06-04 08:29:34 +0530

    Initial Commit

---
+++ src/components/Logo/index.js
@@ -0,0 +1,18 @@
+import React from 'react';
+import './style.css';
+
+/**
+* @author
+* @function Logo
+**/
+
+const Logo = (props) => {
+  return(
+    <div className="logo">
+        <a href="#">Diwakar Singh</a>
+    </div>
+   )
+
+ }
+
+export default Logo
